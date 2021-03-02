94a9e2617d3c0ff5c498e327abdb7aa9f1f4fdfe
Ethan Amar Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2021-03-02 00:14:44 +0000

    Change servlet response to return a string instead of a pre-formatted HTML response.

--- portfolio/src/main/java/com/google/sps/servlets/SimpleStringServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SimpleStringServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /simple-string URL.
- *  This servlet simply returns a simple hard-coded html page containing a header and a paragraph.
+ *  This servlet simply returns a simple hard-coded string.
 */
 @WebServlet("/simple-string")
 public class SimpleStringServlet extends HttpServlet {
@@ -15,7 +15,6 @@ public class SimpleStringServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;");
-        response.getWriter().println("<h1>Server response</h1>");
-        response.getWriter().println("<p>This is a hard-coded string returned by a servlet!</p>");
+        response.getWriter().println("This is a hard-coded string returned by a servlet!");
     }
 }
